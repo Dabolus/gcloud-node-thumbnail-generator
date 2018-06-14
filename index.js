490fb25f3b6b1f7d22bd661a2da277e14956169c
Giorgio Garasto <mail@dabolus.app>
2018-06-14 22:08:09 +0200

    Fix file download

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Storage = require('@google-cloud/storage');
 const Datastore = require('@google-cloud/datastore');
 
-let storage, datastore;
+const bucketName = 'dabolus-bucket';
+let bucket, datastore;
 
 /**
  * Triggered from a change to a Cloud Storage bucket.
@@ -10,19 +11,21 @@ let storage, datastore;
  * @param {!Object} context The context object for the event.
  */
 exports.generateThumbnail = async (file, context) => {
-  storage = storage || new Storage({
+  bucket = bucket || new Storage({
     projectId: process.env.GOOGLE_CLOUD_PROJECT,
-  });
+  }).bucket(bucketName);
   datastore = datastore || new Datastore({
     projectId: process.env.GOOGLE_CLOUD_PROJECT,
   });
 
+  if (file.bucket !== bucketName) {
+    return;
+  }
   try {
     // We expect the file name to be the same as the ID of our project ID on Datastore
     const projectId = file.name.replace(/^.*[\\\/]/g, '').replace(/\.[^.]+$/g, '');
-    // const [buffer] = await file.download();
     console.log(`Generating thumbnail for '${projectId}'...`);
-    console.log('File: ', file, '\nClass: ', file.constructor.name);
+    const [buffer] = await bucket.file(file.name).download();
     // TODO: generate the thumbnail using sqip or node-primitive
   } catch (e) {
     console.error(e);
